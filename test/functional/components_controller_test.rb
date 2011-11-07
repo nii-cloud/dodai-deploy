3a0ac5f22dba9080b4ff32b4f1bd6c091ac3b48c
nii <nii@ubuntu1.(none)>
2011-11-07 14:05:29 +0900

    Add updated controller test

--- test/functional/components_controller_test.rb
+++ test/functional/components_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class ComponentsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should get index" do
+    xhr :post, :index
+    assert_response :success
+    assert_not_nil assigns(:components)
   end
 end
