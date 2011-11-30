d4daf163d8ef0669f2bdca529f63d436f651e898
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2011-11-30 10:50:43 +0900

    Modify the test script for glance to check whether service pids are changing or not.

--- setup-env/puppet/files/glance/test.sh
+++ setup-env/puppet/files/glance/test.sh
@@ -3,15 +3,33 @@
 HOME="/tmp/glance"
 
 #check if glance-api is running
-service glance-api status | grep running
+status glance-api | grep running
 if [ $? != 0 ]; then
     echo "Service glance-api is not started."
     exit 1
 fi
 
-service glance-registry status | grep running
+apt-get install gawk -y
+
+pid1="`status glance-api | awk '{print $4}'`"
+sleep 2
+pid2="`status glance-api | awk '{print $4}'`"
+if [ $pid1 != $pid2 ]; then
+    echo "Service glance-api's pid is changing."
+    exit 1
+fi
+
+status glance-registry | grep running
 if [ $? != 0 ]; then
     echo "Service glance-registry is not started."
+    exit 1
+fi
+
+pid1="`status glance-registry | awk '{print $4}'`"
+sleep 2
+pid2="`status glance-registry | awk '{print $4}'`"
+if [ $pid1 != $pid2 ]; then
+    echo "Service glance-registry's pid is changing."
     exit 1
 fi
 
