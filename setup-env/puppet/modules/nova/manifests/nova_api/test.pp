163344d2b302fba59b6c97cb3a4f252352d4a75e
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2011-12-01 17:22:59 +0900

    Fix the bug, Could not find dependency File[/tmp/nova].

--- setup-env/puppet/modules/nova/manifests/nova_api/test.pp
+++ setup-env/puppet/modules/nova/manifests/nova_api/test.pp
@@ -2,9 +2,18 @@ class nova::nova_api::test {
     $image_file_name = "image_kvm.tgz"
 
     file {
+        "/tmp/nova":
+           ensure => directory,
+           mode => 644;
+
         "/tmp/nova/test.sh":
             alias => "test.sh",
             source => "puppet:///modules/nova/test.sh",
+            require => File["/tmp/nova"];
+
+        "/tmp/nova/$image_file_name":
+            alias => "$image_file_name",
+            source => "puppet:///modules/nova/$image_file_name",
             require => File["/tmp/nova"];
     }
 
@@ -12,12 +21,5 @@ class nova::nova_api::test {
         "/tmp/nova/test.sh $image_file_name $project $user 2>&1":
             alias => "test.sh",
             require => File["test.sh", "$image_file_name"];
-    }
-
-    file {
-        "/tmp/nova/$image_file_name":
-            alias => "$image_file_name",
-            source => "puppet:///modules/nova/$image_file_name",
-            require => File["/tmp/nova"];
     }
 }
