2df72fac3a04b3fc5f0f05ee1af824a6c12977cf
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2011-12-15 19:35:37 +0900

    Make use of hadoop service. Commonize hadoop init files.

--- softwares/hadoop/puppet/manifests/component/install.pp
+++ softwares/hadoop/puppet/manifests/component/install.pp
@@ -5,13 +5,31 @@ define hadoop::component::install() {
 
     if $title == "namenode" {
         exec {
-            "echo Y | $hadoop_home/bin/hadoop namenode -format; USER=root $hadoop_home/bin/hadoop-daemon.sh --config $hadoop_home/conf start $title 2>&1; sudo sysv-rc-conf hadoop-$title on":
-                require => File["core-site", "hdfs-site", "mapred-site", "hadoop-env"]
+            "echo Y | $hadoop_home/bin/hadoop namenode -format":
+                require => File["core-site", "hdfs-site", "mapred-site", "hadoop-env"],
+                notify => Service["hadoop-$title"];
         }
-    } else {
-        exec {
-            "echo Y | USER=root $hadoop_home/bin/hadoop-daemon.sh --config $hadoop_home/conf start $title 2>&1; sudo sysv-rc-conf hadoop-$title on":
-                 require => File["core-site", "hdfs-site", "mapred-site", "hadoop-env"],
-        }
+    }
+
+    file {
+        "/etc/init.d/hadoop-$title":
+            content => template("hadoop/component.erb"),
+            mode => 0644,
+            alias => "hadoop-init";
+    }
+
+    service {
+        "hadoop-$title":
+            ensure => running,
+            require => File["hadoop-init", "core-site", "hdfs-site", "mapred-site", "hadoop-env"];
+    }
+
+    package {
+        sysv-rc-conf:
+    }
+
+    exec {
+        "sysv-rc-conf hadoop-$title on":
+            require => [File["hadoop-init"], Package[sysv-rc-conf]];
     }
 }
