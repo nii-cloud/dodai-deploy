8fdfccbe77fbe474b5e6290f278c0b8df2f8a8c4
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-01-10 11:25:41 +0900

    Add setup for swift to setup-all-in-one.sh. Change rails environment to production in setup-all-in-one.sh.

--- setup-env/setup-all-in-one.sh
+++ setup-env/setup-all-in-one.sh
@@ -4,5 +4,6 @@ git clone https://github.com/nii-cloud/dodai-deploy
 cd dodai-deploy/setup-env
 ./setup.sh server
 ./setup.sh -s `hostname -f` node
+./setup-storage-for-swift.sh loopback /srv/node sdb1 4
 cd ..
-script/start-servers
+script/start-servers production
