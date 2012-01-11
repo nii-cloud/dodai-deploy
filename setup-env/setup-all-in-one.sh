85c706c59e781277dca7a458924f41e66234d111
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-01-11 16:06:38 +0900

    Add git installation when setup all-in-one.

--- setup-env/setup-all-in-one.sh
+++ setup-env/setup-all-in-one.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+apt-get update
+
+apt-get install git -y
 git clone https://github.com/nii-cloud/dodai-deploy
+
 cd dodai-deploy/setup-env
 ./setup.sh server
 ./setup.sh -s `hostname -f` node
 ./setup-storage-for-swift.sh loopback /srv/node sdb1 4
+
 cd ..
 script/start-servers production
