2a41bb21752f862e1ed8e2a07aa1f0d52a0c8740
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-01-13 20:09:39 +0900

    Save osdb.tgz to puppet.

--- lib/tasks/scripts/nova-puppet-init.sh
+++ lib/tasks/scripts/nova-puppet-init.sh
@@ -11,6 +11,6 @@ target_file="/etc/puppet/modules/nova/files/osdb.tgz"
 if [ ! -e $target_file ]; then
   apt-get -y install bzr
   bzr branch lp:horizon/diablo -r 46 osdb
-  tar cvzf /opt/osdb.tgz osdb > /dev/null
+  tar cvzf $target_file osdb > /dev/null
   rm -rf osdb
 fi
