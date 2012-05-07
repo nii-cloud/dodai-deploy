a0ce280872803d683bc276d150c7ac1d679e1138
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-05-07 16:11:18 +0900

    Change presice ec2 image id.

--- test/ec2-smoke-test.sh
+++ test/ec2-smoke-test.sh
@@ -10,4 +10,4 @@ fi
 ./ec2-test-image.sh maverick ami-d6b400d7
 ./ec2-test-image.sh natty ami-fab004fb
 ./ec2-test-image.sh oneiric ami-7c90277d
-./ec2-test-image.sh precise ami-eefb4bef 
+./ec2-test-image.sh precise ami-2cc7772d 
