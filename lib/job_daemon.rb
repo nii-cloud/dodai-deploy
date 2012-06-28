0e77b20fec48a07354b447c5477a1af7fb82c079
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-06-28 11:06:12 +0000

    Parallellize the processing about puppet.

--- lib/job_daemon.rb
+++ lib/job_daemon.rb
@@ -13,19 +13,18 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+require "thread"
+
 class JobDaemon < DaemonSpawn::Base
   def start(args)
     puts "Start daemon"
+
+    @lockers = {}
+    @threads = []
+
     mq = MessageQueueClient.new
     mq.subscribe do |msg_obj|
-      begin 
-        p msg_obj
-        operation_name = msg_obj[:operation]
-        Operations.new(operation_name).invoke msg_obj[:params]
-      rescue Exception => exc
-        puts exc.inspect
-        puts exc.backtrace
-      end
+      @threads << do_in_thread(msg_obj)
     end
 
     puts "Daemon started"
@@ -36,11 +35,34 @@ class JobDaemon < DaemonSpawn::Base
     mq.unsubscribe
     mq.close
 
+    @threads.each{|t| t.join}
+
     puts "Daemon stopped"
   end
 
   def stop
     @stopped = true
     puts "Stop daemon"
+  end
+
+  def do_in_thread(msg_obj)
+    auth_token = msg_obj[:params][:auth_token]
+    @lockers[auth_token] = Mutex::new unless @lockers.has_key? auth_token
+    Thread.new do
+      puts "Thread for #{auth_token}"
+      @lockers[auth_token].synchronize do
+        begin
+          p msg_obj
+          operation_name = msg_obj[:operation]
+          Operations.new(operation_name).invoke msg_obj[:params]
+        rescue Exception => exc
+          puts auth_token
+          puts exc.inspect
+          puts exc.backtrace
+        end        
+
+        @threads.delete self
+      end
+    end
   end
 end
--- lib/mc_utils.rb
+++ lib/mc_utils.rb
@@ -13,25 +13,35 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-require "mcollective"
-include MCollective::RPC
-
-module MCollective
-  class Config
-    attr_writer :topicprefix 
-  end
-end
-
+require "pty"
 
 class McUtils
+  def self.prepare_config_file(auth_token)
+    config_file = open "#{Rails.root.to_s}/config/mc_client.cfg"
+    content = config_file.read
+    config_file.close
+
+    file = Tempfile.new(auth_token)
+    path = file.path
+    begin
+      file.puts(content.sub("TOKEN", auth_token))
+    ensure
+      file.close
+    end
+
+    return path
+  end
+
+  def self.create_cmd(sub_cmd, auth_token)
+    config_file_path = self.prepare_config_file auth_token
+    cmd = "/usr/bin/mco #{sub_cmd} -c #{config_file_path} --dt #{Settings.mcollective.discovery_timeout} --json"
+    puts cmd
+    cmd
+  end
 
   def self.find_hosts(auth_token)
-    mc = rpcclient "rpcutil", :options => self._rpcoptions
-    config = MCollective::Config.instance
-    config.topicprefix = "/topic/#{auth_token}."
-    Rails.logger.debug config.topicprefix
-    result = mc.discover
-    mc.disconnect
+    cmd = self.create_cmd "find", auth_token 
+    result = `#{cmd}`.strip.split "\n"
     result.collect{|item|
       ip, name = item.split ":"
       {:ip => ip, :name => name}
@@ -39,32 +49,28 @@ class McUtils
   end
   
   def self.puppetd_runonce(node_names, auth_token)
-    options = self._rpcoptions
-    options[:filter]["fact"] = [{:fact => "hostname", :value => node_names.join("|"), :operator => "=~"}]
-    options[:verbose] = true
-    options[:timeout] = Settings.mcollective.timeout 
-    options[:disctimeout] = Settings.mcollective.discovery_timeout
-
-    mc = rpcclient "puppetd", :options => options
-    config = MCollective::Config.instance
-    config.topicprefix = "/topic/#{auth_token}."
-    output = mc.runonce :server => Settings.puppet.server, :auth_token => auth_token 
-    mc.disconnect
-  
+    node_names_str = node_names.join "|"
+    cmd = self.create_cmd "rpc puppetd runonce server=#{Settings.puppet.server} auth_token=#{auth_token} -t #{Settings.mcollective.timeout} --wf 'hostname=~" + node_names_str + "'", auth_token
+    PTY.spawn(cmd) do |r, w|
+      output = r.read
+    end
+    puts output
+    output = JSON.parse(output)
+ 
     ret = {}
     p output
     output.each do |item|
       puts item.to_yaml
 
-      status_code = item.results[:statuscode]
+      status_code = item["statuscode"]
       if status_code == 0
-        message = item.results[:data][:output].gsub /\e[\[0-9;]+m/, ""
+        message = item["data"]["output"].gsub /\e[\[0-9;]+m/, ""
         status_code = 1 if /^E: / =~ message or /^err: / =~ message
       else
-        message = item.results[:statusmsg]
+        message = item["statusmsg"]
       end
 
-      name = item.results[:sender].split(":")[1]
+      name = item["sender"].split(":")[1]
       ret[name] = {:status_code => status_code, :message => message}
     end
 
@@ -73,16 +79,5 @@ class McUtils
     end 
 
     ret
-  end
-
-  def self._rpcoptions
-    {:filter=>{"identity"=>[], "fact"=>[], "agent"=>[], "cf_class"=>[]}, 
-     :collective=>nil, 
-     :config=>"/etc/mcollective/client.cfg", 
-     :disctimeout=>Settings.mcollective.discovery_timeout, 
-     :verbose=>false, 
-     :progress_bar=>true, 
-     :timeout=>Settings.mcollective.timeout
-    }
   end
 end
