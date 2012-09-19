aaf3c4a277de6e853080e77cf3577eb98e1a36fb
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-09-19 17:26:27 +0900

    Update external node script.

--- setup-env.centos/puppet/external_nodes.rb
+++ setup-env.centos/puppet/external_nodes.rb
@@ -5,6 +5,7 @@ require 'json'
 require 'net/http'
 require 'yaml'
 require 'active_support/core_ext'
+require 'socket'
 
 hostname = ARGV[0]
 
@@ -13,8 +14,12 @@ parameters = YAML.load_file path
 
 url = "http://localhost:PORT/node_configs/#{hostname}/puppet.json?" + parameters.to_query
 data = Net::HTTP.get_response(URI.parse(url)).body
-
 obj = JSON.parse data
+obj["parameters"]["self_host"] = IPSocket.getaddress(hostname)
+obj["parameters"]["self_host_fqdn"] = hostname
+
+File.open("/var/log/puppet/puppet_#{hostname}.yml", "w") {|f| f.write JSON.pretty_generate obj}
+
 puts YAML.dump obj 
 
 exit 0 
