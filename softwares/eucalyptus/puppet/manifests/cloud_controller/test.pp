754dd1de132261cef00f7de53704fc6e65438ab8
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-11-02 14:41:27 +0900

    Fix bugs related to eucalyptus.

--- softwares/eucalyptus/puppet/manifests/cloud_controller/test.pp
+++ softwares/eucalyptus/puppet/manifests/cloud_controller/test.pp
@@ -1,18 +1,22 @@
 class eucalyptus::cloud_controller::test{
     $image_file_name = "image_kvm.tgz"
 
+    package {
+        gawk:
+    }
+
     file {
         "/var/lib/eucalyptus/test.sh":
-	    alias => "test.sh",
-	    content => template("eucalyptus/test.sh.erb");
+            alias => "test.sh",
+            content => template("eucalyptus/test.sh.erb");
 
-	"/var/lib/eucalyptus/$image_file_name":
-	    alias => "$image_file_name",
-	    source => "puppet:///modules/eucalyptus/$image_file_name";
+        "/var/lib/eucalyptus/$image_file_name":
+            alias => "$image_file_name",
+            source => "puppet:///modules/eucalyptus/$image_file_name";
     }
 
     exec{
         "/var/lib/eucalyptus/test.sh $image_file_name 2>&1":
-	    require => File["test.sh", "$image_file_name"];
+            require => [File["test.sh", "$image_file_name"], Package[gawk]];
     }
 }
