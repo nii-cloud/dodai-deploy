c0c10140fd075d60211de20fa13e99fd0ab10011
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-11-20 15:21:15 +0900

    Fixed bug related to setup all in one.

--- setup-env/setup-all-in-one.sh
+++ setup-env/setup-all-in-one.sh
@@ -12,8 +12,14 @@ fi
 git clone http://github.com/nii-cloud/dodai-deploy
 
 cd dodai-deploy/setup-env
-./setup.sh -x "$http_proxy" server
-./setup.sh -s `hostname -f` -x "$http_proxy" node
+if [ "$http_proxy" != "" ]; then
+  ./setup.sh -x "$http_proxy" server
+  ./setup.sh -s `hostname -f` -x "$http_proxy" node
+else
+  ./setup.sh server
+  ./setup.sh -s `hostname -f` node
+fi
+
 ./setup-storage-for-swift.sh loopback /srv/node sdb1 4
 
 cd ..
