c0a22f18b7947970cf8d4d9bef03f937d84ea9c5
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-11-20 15:09:36 +0900

    Change hadoop version from 0.20.2 to 0.22.0

--- softwares/hadoop/puppet-init.sh
+++ softwares/hadoop/puppet-init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-version="0.20.2"
+version="0.22.0"
 target_file="/etc/puppet/modules/hadoop/files/hadoop-$version.tar.gz"
 if [ ! -e $target_file ]; then
-  wget http://www.us.apache.org/dist/hadoop/common/hadoop-$version/hadoop-$version.tar.gz
+  wget http://www.us.apache.org/dist/hadoop/common/hadoop-$version/hadoop-$version.tar.gz || \
+  wget http://ftp.jaist.ac.jp/pub/apache/hadoop/common/hadoop-$version/hadoop-$version.tar.gz
   mv hadoop-$version.tar.gz $target_file
 fi
--- softwares/hadoop/puppet/manifests/init.pp
+++ softwares/hadoop/puppet/manifests/init.pp
@@ -1,4 +1,4 @@
 class hadoop{
   $home = "/opt/hadoop"
-  $version = "0.20.2"
+  $version = "0.22.0"
 }
