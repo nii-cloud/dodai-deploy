6b30aa0e99a3f844c5df5ae1aa3c75ab0144613f
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-12-19 06:31:05 +0000

    Add proxy settings to puppet.conf

---
+++ setup-env/Ubuntu/set_proxy_to_puppet_conf.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+url = ARGV.shift
+md = url.match /\/\/(.*):(.*)/
+if md
+  host = md[1]
+  port = md[2]
+else
+  md = url.match /\/\/(.*)/
+  host = md[1]
+  port = "80"
+end
+
+`echo "" >> /etc/puppet/puppet.conf`
+`echo http_proxy_host=#{host} >> /etc/puppet/puppet.conf`
+`echo http_proxy_port=#{port} >> /etc/puppet/puppet.conf`
