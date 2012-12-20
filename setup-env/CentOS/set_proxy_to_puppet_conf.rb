f62f8b83c76dc506f4427749b23adca140103c1c
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-12-20 01:09:51 -0500

    Add proxy to puppet.conf for centos.

---
+++ setup-env/CentOS/set_proxy_to_puppet_conf.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+url = ARGV.shift
+md = url.match /\/\/(.*):(.*)/
+if md
+  host = md[1]
+  port = md[2]
+else
+  md = url.match /\/\/(.*)/
+  host = md[1]
+  port = "80"
+end
+
+`echo "    http_proxy_host=#{host}" >> /etc/puppet/puppet.conf`
+`echo "    http_proxy_port=#{port}" >> /etc/puppet/puppet.conf`
