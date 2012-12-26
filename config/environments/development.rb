36f42ef1c774e3683de170429ee686966f12e603
Xiaohua Guan <guanxiaohua2k6@gmail.com>
2012-12-26 09:15:06 +0000

    Change settings of rails.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,6 @@ Deployment::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
   # Do not compress assets
   config.assets.compress = false
   
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Deployment::Application.configure do
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  #config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
@@ -44,7 +44,7 @@ Deployment::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  # config.serve_static_assets = true
+  # config.serve_static_assets = false 
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -62,14 +62,19 @@ Deployment::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  #config.assets.precompile += %w( application.css.sass )
+
   # Compress JavaScript and CSS
-  config.assets.compress = false
-   
+  config.assets.compress = false 
+ 
+  # Expands the lines which load the assets
+  config.assets.debug = true
+  
   # Don't fallback to assets pipeline
-  # config.assets.compile = false 
+  #config.assets.compile = true 
    
   # Generate digests for assets URLs
-  # config.assets.digest = true
+  #config.assets.digest = true
 
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
